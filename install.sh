fabc001efdabef227ab99f304f45bbfdec7330fc
oliveringle <oliver.ingle@gmail.com>
2024-02-04 23:08:41 +0100

    Updates to install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 echo Installing...
 
-curl -o /usr/local/bin/cecho https://raw.githubusercontent.com/OliverIngle/color-echo/main/cecho.sh
+sudo curl -o /usr/local/bin/cecho https://raw.githubusercontent.com/OliverIngle/color-echo/main/cecho.sh
